1e570207b7f442364d3e07ebd4124cf9d7d06c86
Khairalla <161679719+Mahmoudkhairalla@users.noreply.github.com>
2024-03-31 15:16:46 +0700

    name_generator

---
+++ name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the youtube Channel Name Generator:")
+name = input("What is your nickname?\n")
+Channel = input("What is your channel about?\n")
+print("you could name your channel", "(" , Channel , "with" , name , ")")
